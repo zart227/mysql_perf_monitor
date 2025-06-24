6b7e7cc8d6b012305b37c1c9a83034de012cc821
zart227 <zart227@gmail.com>
2025-06-24 06:01:38 +0300

    Logger: improve file/console logging, rotation, secrets protection

--- core/logger.py
+++ core/logger.py
@@ -2,11 +2,12 @@ import logging
 import os
 from datetime import datetime
 from config.config import LOG_TO_FILE, LOG_TO_CONSOLE, LOG_LEVEL
+from logging.handlers import TimedRotatingFileHandler
 
 LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs')
 os.makedirs(LOG_DIR, exist_ok=True)
 
-log_file = os.path.join(LOG_DIR, f"mysql_perf_reporter_{datetime.now().strftime('%Y%m%d')}.log")
+log_file = os.path.join(LOG_DIR, f"mysql_perf_reporter.log")
 
 # Создаем логгер
 logger = logging.getLogger('mysql_perf_reporter')
@@ -21,7 +22,10 @@ formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 
 # Добавляем обработчик для файла (если включено)
 if LOG_TO_FILE:
-    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    file_handler = TimedRotatingFileHandler(
+        log_file, when='midnight', interval=1, backupCount=14, encoding='utf-8', utc=False
+    )
+    file_handler.suffix = "%Y%m%d"
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
